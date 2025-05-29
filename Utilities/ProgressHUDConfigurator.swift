281ff0a951b9b44841bd063c948a55b69763cb4a
SergeySKRV <ap0llyon@yandex.ru>
2025-05-29 23:56:04 +0300

    Delete Utilities directory

--- Utilities/ProgressHUDConfigurator.swift
+++
@@ -1,9 +0,0 @@
-import ProgressHUD
-
-enum ProgressHUDConfigurator {
-    static func configure() {
-        ProgressHUD.animationType = .activityIndicator
-        ProgressHUD.colorHUD = .ypBlack
-        ProgressHUD.colorAnimation = .ypGray
-    }
-}
